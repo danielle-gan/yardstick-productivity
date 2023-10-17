38f04a808a16222504a7858dea814002336b709c
Danielle Gan <DGan@data-mail.com>
2023-10-17 17:05:27 -0400

    still working on form submission functionality

--- docs/work.js
+++ docs/work.js
@@ -6,6 +6,7 @@ const submitButton = document.getElementById('submit-button');
 const question1 = document.getElementById('question-1');
 const question2 = document.getElementById('question-2');
 const question3 = document.getElementById('question-3');
+const questionsForm = document.getElementById('questions-form');
 
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
@@ -25,6 +26,9 @@ modal.style.display = 'block';
 
 
 submitButton.addEventListener('click', () => {
+  
+  questionsForm.style.display = 'flex';
+
   const projectAnswer = document.getElementById('project-input').value;
   const task1Answer = document.getElementById('task-1').value;
   const task2Answer = document.getElementById('task-2').value;
@@ -33,37 +37,18 @@ submitButton.addEventListener('click', () => {
   const maintenance2Answer = document.getElementById('maintenance-2').value;
   const maintenance3Answer = document.getElementById('maintenance-3').value;
 
-  // Create and display labeled divs with answers
-  const projectDiv = document.createElement('div');
-  projectDiv.innerHTML = `<div class="flex"><h2>Focus Project:</h2> <p>${projectAnswer}</p> </div>`;
-
-  const task1Div = document.createElement('div');
-  task1Div.innerHTML = `<div class="flex"><h2>Sticky Task 1:</h2> <p>${task1Answer}</p> </div>`;
-
-  const task2Div = document.createElement('div');
-  task2Div.innerHTML = `<div class="flex"><h2>Sticky Task 2:</h2> <p>${task2Answer}</p> </div>`;
-
-  const task3Div = document.createElement('div');
-  task3Div.innerHTML = `<div class="flex"><h2>Sticky Task 3:</h2> <p>${task3Answer}</p> </div>`;
-
-  const maintenance1Div = document.createElement('div');
-  maintenance1Div.innerHTML = `<div class="flex"><h2>Sticky Task 1:</h2> <p>${maintenance1Answer}</p> </div>`;
-
-  const maintenance2Div = document.createElement('div');
-  maintenance2Div.innerHTML = `<div class="flex"><h2>Sticky Task 2:</h2> <p>${maintenance2Answer}</p> </div>`;
-
-  const maintenance3Div = document.createElement('div');
-  maintenance3Div.innerHTML = `<div class="flex"><h2>Sticky Task 3:</h2> <p>${maintenance3Answer}</p> </div>`;
-
-  answersContainer.appendChild(projectDiv);
-  answersContainer.appendChild(task1Div);
-  answersContainer.appendChild(task2Div);
-  answersContainer.appendChild(task3Div);
-  answersContainer.appendChild(maintenance1Div);
-  answersContainer.appendChild(maintenance2Div);
-  answersContainer.appendChild(maintenance3Div);
+  // Update labels with user input
+  document.getElementById('project-output').nextElementSibling.textContent = projectAnswer;
+  document.getElementById('task1').nextElementSibling.textContent = task1Answer;
+  document.getElementById('task2').nextElementSibling.textContent = task2Answer;
+  document.getElementById('task3').nextElementSibling.textContent = task3Answer;
+  document.getElementById('maintenance1').nextElementSibling.textContent = maintenance1Answer;
+  document.getElementById('maintenance2').nextElementSibling.textContent = maintenance2Answer;
+  document.getElementById('maintenance3').nextElementSibling.textContent = maintenance3Answer;
+  
+  // Reset form input values
+  form.reset();
 
   // Close the modal
   modal.style.display = 'none';
 });
-
